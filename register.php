bcac2ce24cc4e859bdb6e64c09635b3c611b94ba
olukotunmonsurat <olukotunmonsurat1234@gmail.com>
2025-06-08 23:31:09 +0100

    adjusted register dark mode

--- register.php
+++ register.php
@@ -58,9 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <body class="<?php echo $darkMode ? 'dark-mode' : ''; ?>">
 
 
-  <a href="?darkmode=<?php echo $darkMode ? '0' : '1'; ?>" class="dark-toggle">
-    <?php echo $darkMode ? '☀️ Light Mode' : '🌙 Dark Mode'; ?>
-  </a>
+ 
     <div class="form-box register">
        
         <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
